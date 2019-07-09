7ae1e52879f946a53870402f8ec0fb8db68c31ec
KTyH <archlis21@gmail.com>
2019-07-09 14:20:47 +0300

    +

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,39 +1,105 @@
 <template>
   <div class="home">
-    <div class="grid-container">
-      <div class="cc"></div>
-      <div class="classes-wrapper">
-        <div class="assassin class">Убийца</div>
-        <div class="blademaster class">Мастер клинка</div>
-        <div class="brawler class">Дебошир</div>
-        <div class="elementalist class">Элементалист</div>
-        <div class="guardian class">Страж</div>
-        <div class="gunslinger class">Стрелок</div>
-        <div class="knight class">Рыцарь</div>
-        <div class="ranger class">Рейнджер</div>
-        <div class="shapeshifter class">Перевёртыш</div>
-        <div class="sorcerer class">Чародей</div>
+    <div class="champions-table">
+      <div class="classes">
+        <div class="origins"></div>
+        <div class="class">убийца</div>
+        <div class="class">мастер клинка</div>
+        <div class="class">дебошир</div>
+        <div class="class">элементалист</div>
+        <div class="class">страж</div>
+        <div class="class">стрелок</div>
+        <div class="class">рыцарь</div>
+        <div class="class">рэйнджер</div>
+        <div class="class">перевертыш</div>
+        <div class="class">чародей</div>
       </div>
-      <div class="origins-wrapper">
-        <div class="origin demon">Демон</div>
-        <div class="origin dragon">Дракон</div>
-        <div class="origin exile">Изгнанник</div>
-        <div class="origin glacial">Оледенелый</div>
-        <div class="origin robot">Робот</div>
-        <div class="origin imperial">Имперец</div>
-        <div class="origin noble">Пристократ</div>
-        <div class="origin ninja">Ниндзя</div>
-        <div class="origin pirate">Пират</div>
-        <div class="origin phantom">Призрак</div>
-        <div class="origin wild">Одичалый</div>
-        <div class="origin void">Обитатель бездны</div>
-        <div class="origin yordle">Йордл</div>
-      </div>
-      <div class="champions">
-        <div class="champion">
-          <img src="../assets/images/champions/Nidalee.png" alt="">
+
+      <div class="champions-table-row">
+        <div class="origins">
+          <div class="origin">демон</div>
+          <div class="origin">дракон</div>
+          <div class="origin">изгнанник</div>
+          <div class="origin">оледенелый</div>
+          <div class="origin">робот</div>
+          <div class="origin">имперец</div>
+          <div class="origin">аристократ</div>
+          <div class="origin">ниндзя</div>
+          <div class="origin">пират</div>
+          <div class="origin">призрак</div>
+          <div class="origin">одичалый</div>
+          <div class="origin">обитатель бездны</div>
+          <div class="origin">йордл</div>
+        </div>
+
+        <div class="champions">
+          <div class="champions-row">
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+          </div>
+          <div class="champions-row">
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+            <div class="champion">
+              <img src="../assets/images/champions/Nidalee.png" alt="">
+            </div>
+          </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -46,136 +112,66 @@ export default {
 
 <style lang="scss">
 
-  .grid-container {
-    display: grid;
-    grid-template-columns: 0.5fr 1.5fr 1fr 1fr;
-    grid-template-rows: 0.6fr 1.4fr 1fr;
-    grid-template-areas: "cc classes-wrapper classes-wrapper classes-wrapper" "origins-wrapper champions champions champions" "origins-wrapper champions champions champions";
+  $rowHeight: 100px;
+
+  .champions-table {
+    display: flex;
+    flex-direction: column;
+    width: 1000px;
+    min-width: 1000px;
+    margin: 0 auto;
   }
 
-  .cc {
-    grid-area: cc;
+  .champions-table-row {
+    display: flex;
   }
 
-  .classes-wrapper {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    grid-template-areas: "assassin blademaster brawler elementalist guardian knight ranger gunslinger shapeshifter sorcerer"
-    "assassin blademaster brawler elementalist guardian knight ranger gunslinger shapeshifter sorcerer";
-    grid-area: classes-wrapper;
+  .classes {
+    display: flex;
   }
 
-  .assassin {
-    grid-area: assassin;
+  .origins {
+    width: 100px;
+    min-width: 100px;
   }
 
-  .blademaster {
-    grid-area: blademaster;
+  .origin {
+    height: $rowHeight;
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-weight: bold;
+    text-transform: uppercase;
   }
 
-  .brawler {
-    grid-area: brawler;
-  }
-
-  .elementalist {
-    grid-area: elementalist;
-  }
-
-  .guardian {
-    grid-area: guardian;
-  }
-
-  .knight {
-    grid-area: knight;
-  }
-
-  .ranger {
-    grid-area: ranger;
-  }
-
-  .gunslinger {
-    grid-area: gunslinger;
-  }
-
-  .shapeshifter {
-    grid-area: shapeshifter;
-  }
-
-  .sorcerer {
-    grid-area: sorcerer;
-  }
-
-  .origins-wrapper {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-areas: "demon demon demon" "dragon dragon dragon" "exile exile exile" "glacial glacial glacial" "robot robot robot" "imperial imperial imperial" "noble noble noble" "ninja ninja ninja" "ninja ninja ninja" "pirate pirate pirate" "phantom phantom phantom" "wild wild wild" "void void void" "yordle yordle yordle";
-    grid-area: origins-wrapper;
-  }
-
-  .demon {
-    grid-area: demon;
-  }
-
-  .dragon {
-    grid-area: dragon;
-  }
-
-  .exile {
-    grid-area: exile;
-  }
-
-  .glacial {
-    grid-area: glacial;
-  }
-
-  .robot {
-    grid-area: robot;
-  }
-
-  .imperial {
-    grid-area: imperial;
-  }
-
-  .noble {
-    grid-area: noble;
-  }
-
-  .ninja {
-    grid-area: ninja;
-  }
-
-  .ninja {
-    grid-area: ninja;
-  }
-
-  .pirate {
-    grid-area: pirate;
-  }
-
-  .phantom {
-    grid-area: phantom;
-  }
-
-  .wild {
-    grid-area: wild;
-  }
-
-  .void {
-    grid-area: void;
-  }
-
-  .yordle {
-    grid-area: yordle;
+  .class {
+    width: 10%;
+    height: 100px;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    text-transform: uppercase;
   }
 
   .champions {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-areas: ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . ." ". . . . . . . . . .";
-    grid-area: champions;
+    flex: 1;
+  }
+
+  .champions-row {
+    display: flex;
+  }
+
+  .champion {
+    width: 10%;
+    height: $rowHeight;
+    padding: 5px;
+    border: 3px solid #000;
+    img {
+      width: 100%;
+    }
   }
 
 </style>
